f8fcfb915cf3f27b456618a78f80039b35726a6c
Jennifer <jenniferpeterson@pursuit.org>
2024-01-16 13:42:51 -0500

    created Footer and About Components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return <Link to="/about">About the Developers</Link>;
+};
+
+export default Footer;
