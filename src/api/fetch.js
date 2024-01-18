50ead3ffbc3865c34a13706a409c723d55b36071
Jennifer <jenniferpeterson@pursuit.org>
2024-01-18 13:03:38 -0500

    created the getOneStudent function

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -3,3 +3,7 @@ const URL = import.meta.env.VITE_BASE_API_URL;
 export const getAllStudents = () => {
   return fetch(`${URL}/api/students`).then((response) => response.json());
 };
+
+export const getOneStudent = (id) => {
+  return fetch(`${URL}/api/students/${id}`).then((response) => response.json());
+};
