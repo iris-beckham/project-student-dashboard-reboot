48f6fa308caf220cacab703ce3e94fb84f7ffb5e
Jennifer <jenniferpeterson@pursuit.org>
2024-01-18 12:41:35 -0500

    created a function to format the students DOB

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,27 @@ export const Home = () => {
     getAllStudents().then((data) => setStudents(data));
   }, []);
 
+  // const dob = "2"
+  const formattedDateOfBirth = (dob) => {
+    // Parse the date string
+    const dobArray = dob.split("/");
+    const month = parseInt(dobArray[0], 10);
+    const day = parseInt(dobArray[1], 10);
+    const year = parseInt(dobArray[2], 10);
+
+    // Create a Date object
+    const formattedDate = new Date(year, month - 1, day);
+
+    // Format the date to "Month day, Year"
+    const options = { month: "long", day: "numeric", year: "numeric" };
+    const result = formattedDate.toLocaleDateString("en-US", options);
+    return result;
+  };
+
   return (
     <div>
+      <h1>All Students</h1>
+      <h2>Total Students: {students.length}</h2>
       {students.map(({ id, names, username, dob, profilePhoto }) => (
         <li key={id}>
           <img src={profilePhoto} alt={names.preferredName} />
@@ -16,7 +35,7 @@ export const Home = () => {
             {`${names.preferredName} ${names.middleName} ${names.surname} `}
           </h2>
           <p>{username}</p>
-          <p>{dob}</p>
+          <p>{formattedDateOfBirth(dob)}</p>
         </li>
       ))}
     </div>
