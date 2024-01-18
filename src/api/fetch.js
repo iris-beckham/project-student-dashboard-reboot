2a973f04801eef1470b29a1273e2e0a65d5798f2
Jennifer <jenniferpeterson@pursuit.org>
2024-01-18 13:21:10 -0500

    fixed the api in our fetch by removing /api/students

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,9 +1,9 @@
 const URL = import.meta.env.VITE_BASE_API_URL;
 
 export const getAllStudents = () => {
-  return fetch(`${URL}/api/students`).then((response) => response.json());
+  return fetch(`${URL}`).then((response) => response.json());
 };
 
 export const getOneStudent = (id) => {
-  return fetch(`${URL}/api/students/${id}`).then((response) => response.json());
+  return fetch(`${URL}/${id}`).then((response) => response.json());
 };
