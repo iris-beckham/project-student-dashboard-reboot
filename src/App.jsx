7805ac7adabaaf7f67b27ffc2587751df41b73fa
Jennifer <jenniferpeterson@pursuit.org>
2024-01-18 12:56:15 -0500

    created a route for the individual student

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import About from "./components/About";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Aside } from "./components/Aside";
+// import { getAllStudents } from "./api/fetch";
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
+        <Route path="/">
+          <Route index element={<Home />} />
+          {/* <Route path=":id" element={<Student />} /> */}
+        </Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
       <Aside />
