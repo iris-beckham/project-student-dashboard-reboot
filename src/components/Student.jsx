47dda90d549a651fa09734d99316662cdf07240b
Jennifer <jenniferpeterson@pursuit.org>
2024-01-18 13:22:59 -0500

    we grabbed the ID from the path using useParams to use in our getOneStudent function inside of the useEffect

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,4 +1,17 @@
+import { getOneStudent } from "../api/fetch";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Student = () => {
+  const { id } = useParams();
+  //   console.log(id);
+  const [student, setStudent] = useState([]);
+
+  useEffect(() => {
+    getOneStudent(id).then((data) => setStudent(data));
+  }, []);
+
+  //   console.log();
   return <div>Student</div>;
 };
 
