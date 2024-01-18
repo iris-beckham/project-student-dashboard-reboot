4699fd77aeafd6845bca86d90a3aee9a958a767a
Jennifer <jenniferpeterson@pursuit.org>
2024-01-18 14:57:54 -0500

    using the formattedDateOfBirth fx prop and we are rendering the students info to the student page

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -2,17 +2,39 @@ import { getOneStudent } from "../api/fetch";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-const Student = () => {
+const Student = ({ formattedDateOfBirth }) => {
   const { id } = useParams();
+
   //   console.log(id);
-  const [student, setStudent] = useState([]);
+  const [student, setStudent] = useState({
+    names: {
+      preferredName: "",
+      middleName: "",
+      surname: "",
+    },
+    username: "",
+    dob: "",
+    profilePhoto: "",
+  });
 
   useEffect(() => {
     getOneStudent(id).then((data) => setStudent(data));
   }, []);
 
-  //   console.log();
-  return <div>Student</div>;
+  //   console.log(student);
+  const { names, username, dob, profilePhoto } = student;
+
+  return (
+    <div>
+      <img src={profilePhoto} alt={`${names.preferredName}`} />
+
+      <div>
+        <p>{`${names.preferredName} ${names.middleName} ${names.surname}`}</p>
+        <p>{`${username}`}</p>
+        <p>{formattedDateOfBirth(dob)}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Student;
