c767fffd682e91803a044f5cbea497abda5a6393
Iris Beckham <irisdancer3@gmail.com>
2024-01-19 11:06:24 -0800

    added a state for the ascending/descending cohorts and updated the button accordingly

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 
 export const Aside = ({ handleChange }) => {
+  //ascending/descending state
+  //true = ascending (2025->2026), false = descending (2026->2025)
+  const [sortingDirection, setSortingDirection] = useState(true);
   const cohortArr = ['All Students', 'Winter 2025', 'Spring 2025', 'Summer 2025', `Fall 2025`, `Winter 2026`, `Spring 2026`, `Summer 2026`, `Fall 2026`]
 
 
   return (
     <>
       <h2>Choose A Class By Start Date</h2>
-      <button>Sort Descending By Year</button>
+      <button onClick={() => setSortingDirection(!sortingDirection)}>Sort {sortingDirection ? 'Ascending' : 'Descending'} By Year</button>
       <ul>
         {cohortArr.map(elem => {
           return (
