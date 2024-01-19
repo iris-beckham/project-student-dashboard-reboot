60e494efdd39031cfa4014d6789a86c176a1c2de
Jennifer <jenniferpeterson@pursuit.org>
2024-01-19 18:52:18 -0500

    added code to clear the input field after hitting the submit button

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,35 +1,50 @@
 import { useState } from "react";
 
 const CommentForm = ({ comments, setComments }) => {
-    const [comment, setComment] = useState('');
-    const [author, setAuthor] = useState('');
+  const [comment, setComment] = useState("");
+  const [author, setAuthor] = useState("");
 
-    const handleAuthor = (e) => {
-        setAuthor(e.target.value)
-    }
+  // handleAuthor
+  const handleAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
 
-    const handleComment = (e) => {
-        setComment(e.target.value)
-    }
+  // handleComment
+  const handleComment = (e) => {
+    setComment(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newCommentArray = [...comments];
-        newCommentArray.push({ author: author, comment: comment });
-        setComments([...newCommentArray]);
-    }
+  // handleSubmit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newCommentArray = [...comments];
+    newCommentArray.push({ author: author, comment: comment });
+    setComments([...newCommentArray]);
+    setComment("");
+    setAuthor("");
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="author">
-                <input onChange={handleAuthor} type="text" placeholder="author" value={author} />
-            </label>
-            <label htmlFor="comment">
-                <textarea onChange={handleComment} type="text" placeholder="comment" value={comment} />
-            </label>
-            <button type="submit" >Submit</button>
-        </form>
-    )
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="author">
+        <input
+          onChange={handleAuthor}
+          type="text"
+          placeholder="author"
+          value={author}
+        />
+      </label>
+      <label htmlFor="comment">
+        <textarea
+          onChange={handleComment}
+          type="text"
+          placeholder="comment"
+          value={comment}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
-export default CommentForm
+export default CommentForm;
