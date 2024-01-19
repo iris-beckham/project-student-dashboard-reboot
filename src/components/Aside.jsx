abbc383aab7aa9afdeb787aa52b06660008c6169
Iris Beckham <irisdancer3@gmail.com>
2024-01-19 10:51:15 -0800

    implemented a link on each cohort on the aside, routing to the home

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom"
 
 
 export const Aside = ({ handleChange }) => {
   const cohortArr = ['Winter 2025', 'Spring 2025', 'Summer 2025', `Fall 2025`, `Winter 2026`, `Spring 2026`, `Summer 2026`, `Fall 2026`]
 
-  
+
   return (
     <>
       <h2>Choose A Class By Start Date</h2>
       <button>Sort Descending By Year</button>
       <ul>
-          {cohortArr.map(elem => {
-            return <li key={elem} onClick={handleChange}>{elem}</li>
-          })}
+        {cohortArr.map(elem => {
+          return (
+            <Link key={elem} to='/'>
+              <li onClick={handleChange}>{elem}</li>
+            </Link>)
+        })}
       </ul>
     </>
   )
