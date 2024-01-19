8059b72df9d05c22476b723c0be72f1aec54217f
Iris Beckham <irisdancer3@gmail.com>
2024-01-19 13:08:42 -0800

    store authors and comments into a state of comments

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,20 +1,33 @@
+import { useState } from "react";
 
+const CommentForm = ({ comments, setComments }) => {
+    const [comment, setComment] = useState('');
+    const [author, setAuthor] = useState('');
 
-const CommentForm = () => {
+    const handleAuthor = (e) => {
+        setAuthor(e.target.value)
+    }
+
+    const handleComment = (e) => {
+        setComment(e.target.value)
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        const newCommentArray = [...comments];
+        newCommentArray.push({ author: author, comment: comment });
+        setComments([...newCommentArray]);
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <h2>1 on 1 notes</h2>
             <label htmlFor="author">
-                <input type="text" placeholder="author" />
+                <input onChange={handleAuthor} type="text" placeholder="author" value={author} />
             </label>
             <label htmlFor="comment">
-                <textarea type="text" placeholder="comment" />
+                <textarea onChange={handleComment} type="text" placeholder="comment" value={comment} />
             </label>
-            <button type="submit">Submit</button>
+            <button type="submit" >Submit</button>
         </form>
     )
 }
