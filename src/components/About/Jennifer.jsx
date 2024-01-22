e43eb4c1f2b316fbb41bd8991779822174a4cd80
Jennifer <jenniferpeterson@pursuit.org>
2024-01-22 13:08:20 -0500

    completed my about page

--- src/components/About/Jennifer.jsx
+++ src/components/About/Jennifer.jsx
@@ -1,7 +1,36 @@
 const Jennifer = () => {
-    return (
-        <div>Jennifer</div>
-    )
-}
+  return (
+    <div>
+      <img
+        src="https://xsgames.co/randomusers/avatar.php?g=male&minimum_age=38&maximum_age=48"
+        alt="photo of Jennifer Peterson"
+      />
+      <p>
+        Fun fact: I ❤️ to TRAVEL✈️, trying new FOODS 🍣 and I am currently
+        teaching myself to COOK 👩🏿‍🍳 (concentrating on plant-based meals 🥗)
+      </p>
+      <p>How to reach me 👩🏿‍💻</p>
+      <ul>
+        <a href="mailto:jenniferbushpeterson@gmail.com">
+          <li>Email</li>
+        </a>
+        <a
+          href="https://github.com/JenniferPeterson1203"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <li>GitHub</li>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jennifer--peterson/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <li>LinkedIn</li>
+        </a>
+      </ul>
+    </div>
+  );
+};
 
-export default Jennifer
+export default Jennifer;
