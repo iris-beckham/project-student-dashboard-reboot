4c78493b16568ebaf5d6e8e9ab7662653f7efe74
Iris Beckham <irisdancer3@gmail.com>
2024-01-23 09:10:43 -0800

    filtered form and states

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -13,9 +13,37 @@ export const Aside = ({ handleChange }) => {
     cohortArr = [cohortArr[0], ...cohortArr.slice(1).reverse()]
   }
 
+  const [resume, setResume] = useState(false);
+  const [linkedin, setLinkedin] = useState(false);
+  const [github, setGithub] = useState(false);
+  const [interview, setInterview] = useState(false);
+  const [codewars, setCodewars] = useState(false);
+  const [onTrack, setOnTrack] = useState(false);
+
+  const handleResumeCheckbox = (e) => {
+    setResume(e.target.checked);
+  }
+  const handleLinkedinCheckbox = (e) => {
+    setLinkedin(e.target.checked);
+  }
+  const handleGithubCheckbox = (e) => {
+    setGithub(e.target.checked);
+  }
+  const handleInterviewCheckbox = (e) => {
+    setInterview(e.target.checked);
+  }
+  const handleCodewarsCheckbox = (e) => {
+    setCodewars(e.target.checked);
+  }
+  const handleOnTrackCheckbox = (e) => {
+    setOnTrack(e.target.checked);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <>
+    <aside>
       <h2>Choose A Class By Start Date</h2>
       <button onClick={() => setSortingDirection(!sortingDirection)}>Sort {sortingDirection ? 'Descending' : 'Ascending'} By Year</button>
       <ul className="aside-ul">
@@ -26,6 +54,54 @@ export const Aside = ({ handleChange }) => {
             </Link>)
         })}
       </ul>
-    </>
+      <form onSubmit={handleSubmit}>
+        <h3>Filters:</h3>
+        <label htmlFor="">
+          <input type="checkbox"
+            name="resume"
+            value={resume}
+            onChange={handleResumeCheckbox}
+          />
+          Certified Resume
+        </label>
+        <label htmlFor="">
+          <input type="checkbox"
+            name="linkedin"
+            value={linkedin}
+            onChange={handleLinkedinCheckbox}
+          />
+          Certified LinkedIn
+        </label>
+        <label htmlFor="">
+          <input type="checkbox"
+            value={github}
+            onChange={handleGithubCheckbox}
+          />
+          Certified GitHub
+        </label>
+        <label htmlFor="">
+          <input type="checkbox"
+            value={interview}
+            onChange={handleInterviewCheckbox}
+          />
+          Certified Mock Interview
+        </label>
+        <label htmlFor="">
+          <input type="checkbox"
+            value={codewars}
+            onChange={handleCodewarsCheckbox}
+          />
+          Codewars score 850 and over
+        </label>
+        <label htmlFor="">
+          <input type="checkbox"
+            value={onTrack}
+            onChange={handleOnTrackCheckbox}
+          />
+          On track
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </aside>
   )
 }
